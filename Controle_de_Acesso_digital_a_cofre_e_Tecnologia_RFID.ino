f661f623418ce396afb7497d4eb651e5d590b181
thales-fernandes <53984379+thales-fernandes@users.noreply.github.com>
2019-11-11 11:42:35 -0300

    Update Controle_de_Acesso_digital_a_cofre_e_Tecnologia_RFID.ino

--- Controle_de_Acesso_digital_a_cofre_e_Tecnologia_RFID.ino
+++ Controle_de_Acesso_digital_a_cofre_e_Tecnologia_RFID.ino
@@ -1,53 +1,50 @@
-//Programa: Sistema de trava eletrica com RFID
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Servo.h>
  
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-
+Servo myServo; //define servo name
+ 
 void setup() 
 {
-  Serial.begin(9600);   // Inicia a serial
-  SPI.begin();      // Inicia  SPI bus
-  mfrc522.PCD_Init();   // Inicia MFRC522
-  Serial.println("Aproxime o seu cartao do leitor...");
-  Serial.println();
-  pinMode(2, OUTPUT);
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  myServo.attach(3); //servo pin
+  myServo.write(0); //servo start position
 }
-
 void loop() 
 {
-  // Procura por cartao RFID
+  // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
   }
-  // Seleciona o cartao RFID
+  // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
     return;
   }
-  //Mostra UID na serial
-  Serial.print("UID da tag :");
-  String conteudo= "";
-  byte letra;
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
-     conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
-  Serial.print("Mensagem : ");
-  conteudo.toUpperCase();
-
-  if (conteudo.substring(1) == "F3 DE 90 55") //UID 1 - Cartao
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "1B D3 4A 1A") //change here the UID of the card/cards that you want to give access
   {
-    Serial.println("Ola EPAMINONDAS !");
-    Serial.println();
-    digitalWrite(2, HIGH); // ativa rele, abre a trava solenoide
-    delay(3000);           // espera 3 segundos
-    digitalWrite(2, LOW);  // desativa rele, fecha a trava solenoide
+    myServo.write(180);
+    delay(5000);
+    myServo.write(0);
   }
+}
